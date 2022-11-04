43671683d6b9661ff19975e51df9af1cf2a1de3a
Escarcega <said.escarcega@accenture.com>
2022-11-04 10:04:35 -0600

    Project init

---
+++ src/test/java/accenture/training/springbootdi/SpringBootDiApplicationTests.java
@@ -0,0 +1,13 @@
+package accenture.training.springbootdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
